0933368271ec58e9b26bdb7c721d17fcf62e0f05
Devhell69 <paelalit@gmail.com>
2024-01-11 05:59:51 +0700

    edit title 3

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect, useContext } from 'react'
-import Contextpage from '../Contextpage';
-import {Helmet} from "react-helmet";
+import React, { useEffect, useContext } from "react";
+import Contextpage from "../Contextpage";
+import { Helmet } from "react-helmet";
 
 const Genre = () => {
-  const { fetchGenre, activegenre, setActiveGenre, genres, setMovies, page, setPage, filteredGenre } = useContext(Contextpage);    
-
+  const {
+    fetchGenre,
+    activegenre,
+    setActiveGenre,
+    genres,
+    setMovies,
+    page,
+    setPage,
+    filteredGenre,
+  } = useContext(Contextpage);
 
   useEffect(() => {
-      fetchGenre();  // Fetching Genres on Initial Render.
-  }, [])
-
+    fetchGenre(); // Fetching Genres on Initial Render.
+  }, []);
 
   // const filterFunc = () => {
   //     if (activegenre === 0) {
@@ -21,28 +28,28 @@ const Genre = () => {
   //         setFiltered(filteredgenre);
   //     }
   // }
-  
+
   return (
     <>
-    <Helmet>
-        <title>React Movies | API</title>
-    </Helmet>
+     
 
-    <div className='flex flex-wrap justify-center px-2'>
-        {
-            genres.map((genre) => (
+      <div className="flex flex-wrap justify-center px-2">
+        {genres.map((genre) => (
+          <button
+            onClick={() => setActiveGenre(genre.id)}
+            className={
+              activegenre === genre.id
+                ? "active px-4 py-2 m-2 text-[15px] text-white font-semibold rounded-3xl"
+                : "px-4 py-2 m-2 text-[15px] bg-slate-800 text-white font-semibold rounded-3xl"
+            }
+            key={genre.id}
+          >
+            {genre.name}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
 
-                <button
-                    onClick={() => setActiveGenre(genre.id)}
-                    className={activegenre === genre.id ? 'active px-4 py-2 m-2 text-[15px] text-white font-semibold rounded-3xl' : 'px-4 py-2 m-2 text-[15px] bg-slate-800 text-white font-semibold rounded-3xl'} key={genre.id}>
-                    {genre.name}
-                </button>
-
-            ))
-        }
-        </div>
-        </>
-  )
-}
-
-export default Genre
+export default Genre;
